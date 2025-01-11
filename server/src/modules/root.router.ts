cfd5dd5e10987d1629255558c5821a9be193c482
Trishan Wagle <mailtotrishan@gmail.com>
2025-01-11 21:07:10 +0545

    fix: unique name issue on workspace and project, task schema

--- server/src/modules/root.router.ts
+++ server/src/modules/root.router.ts
@@ -3,13 +3,15 @@ import { authRouter } from "./auth/auth.routes";
 import { workspaceRouter } from "./workspace/workspace.routes";
 import { isAuthenticated } from "@/middlewares/isAuthenticated";
 import { memberRouter } from "./member/member.routes";
-import projectsRouter from "./project/project.routes";
+import { projectRouter } from "./project/project.routes";
+import { taskRouter } from "./task/task.routes";
 
 const rootRouter = Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/workspaces", isAuthenticated, workspaceRouter);
 rootRouter.use("/members", memberRouter);
-rootRouter.use("/projects", projectsRouter);
+rootRouter.use("/projects", projectRouter);
+rootRouter.use("/tasks", taskRouter);
 
 export { rootRouter };
---
+++ server/src/modules/task/task.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const taskRouter = Router();
+
+export { taskRouter };
