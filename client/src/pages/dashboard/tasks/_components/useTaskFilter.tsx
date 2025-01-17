edaa09d14a37a2925cf2c99870e4bd212a48d263
Nischaymrzn <nischaymaharjann@gmail.com>
2025-01-17 21:22:24 +0545

    feat: task filter

---
+++ client/src/pages/dashboard/tasks/_components/useTaskFilter.tsx
@@ -0,0 +1,13 @@
+import { parseAsString, parseAsStringEnum, useQueryStates } from "nuqs";
+import { Priority, TaskStatus } from "../_schemas";
+
+export const useTaskFilter = () => {
+    return useQueryStates({
+        projectId: parseAsString,
+        status: parseAsStringEnum(Object.values(TaskStatus)),
+        priority: parseAsStringEnum(Object.values(Priority)),
+        assigneeId: parseAsString,
+        search: parseAsString,
+        dueDate: parseAsString,
+    });
+};
