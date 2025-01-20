3199492916586d3af6feef393c60d6c218046a77
Trishan Wagle <mailtotrishan@gmail.com>
2025-01-20 21:17:27 +0545

    feat: refactor

--- client/src/pages/dashboard/tasks/_components/EditTaskForm.tsx
+++ client/src/pages/dashboard/tasks/_components/EditTaskForm.tsx
@@ -3,342 +3,340 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { DottedSeparator } from "@/components/ui/dotted-separator";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { DatePicker } from "@/components/DatePicker"; 
+import { DatePicker } from "@/components/DatePicker";
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { cn } from "@/lib/utils";
-import { ProjectAvatar } from "../../projects/_components/ProjectAvatar"; 
-import { MemberAvatar } from "../../workspaces/_components/MemberAvatar"; 
+import { ProjectAvatar } from "../../projects/_components/ProjectAvatar";
+import { MemberAvatar } from "../../workspaces/_components/MemberAvatar";
 import {
-	type CreateTaskSchema,
-	createTaskSchema,
-	FibonacciSeries,
-	Priority,
+  type CreateTaskSchema,
+  createTaskSchema,
+  FibonacciSeries,
+  Priority,
 } from "../_schemas/index";
 
 import { TaskStatus, Task } from "../_schemas/index";
 import { useUpdateTask } from "@/hooks/useTasks";
 
 interface EditTaskFormProps {
-	onCancel?: () => void;
-	projectOptions: {
-		id: string;
-		name: string;
-		imageUrl: string;
-	}[];
-	memberOptions: {
-		id: string;
-		name: string;
-	}[];
-	initialValues: Task;
+  onCancel?: () => void;
+  projectOptions: {
+    id: string;
+    name: string;
+    imageUrl: string;
+  }[];
+  memberOptions: {
+    id: string;
+    name: string;
+  }[];
+  initialValues: Task;
 }
 
 export const EditTaskForm = ({
-	onCancel,
-	memberOptions,
-	projectOptions,
-	initialValues,
+  onCancel,
+  memberOptions,
+  projectOptions,
+  initialValues,
 }: EditTaskFormProps) => {
-	const { mutate, isPending } = useUpdateTask();
-	const form = useForm<CreateTaskSchema>({
-		resolver: zodResolver(
-			createTaskSchema.omit({ workspaceId: true, description: true })
-		),
-		defaultValues: {
-			...initialValues,
-			dueDate: initialValues.dueDate
-				? new Date(initialValues.dueDate)
-				: undefined,
-		},
-	});
+  const { mutate, isPending } = useUpdateTask();
+  const form = useForm<CreateTaskSchema>({
+    resolver: zodResolver(
+      createTaskSchema.omit({ workspaceId: true, description: true }),
+    ),
+    defaultValues: {
+      ...initialValues,
+      dueDate: initialValues.dueDate
+        ? new Date(initialValues.dueDate)
+        : undefined,
+    },
+  });
 
-	const onSubmit = (values: CreateTaskSchema) => {
-		console.log(values)
-		mutate(
-			{ taskId: initialValues.id, data : values },
-			{
-				onSuccess: () => {
-					form.reset();
-					onCancel?.();
-				},
-			}
-		);
-	};
+  const onSubmit = (values: CreateTaskSchema) => {
+    console.log(values);
+    mutate(
+      { taskId: initialValues.id, data: values },
+      {
+        onSuccess: () => {
+          form.reset();
+          onCancel?.();
+        },
+      },
+    );
+  };
 
-	return (
-		<Card className="size-full border-none shadow-none">
-			<CardHeader className="flex p-7">
-				<CardTitle className="text-xl font-bold">Edit a task</CardTitle>
-			</CardHeader>
+  return (
+    <Card className="size-full border-none shadow-none">
+      <CardHeader className="flex p-7">
+        <CardTitle className="text-xl font-bold">Edit a task</CardTitle>
+      </CardHeader>
 
-			<div className="px-7">
-				<DottedSeparator />
-			</div>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
 
-			<CardContent className="p-7">
-				<Form {...form}>
-					<form onSubmit={form.handleSubmit(onSubmit)}>
-						<div className="flex flex-col gap-y-4">
-							<FormField
-								control={form.control}
-								name="name"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Task name</FormLabel>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Task name</FormLabel>
 
-										<FormControl>
-											<Input {...field} placeholder="Enter task name" />
-										</FormControl>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter task name" />
+                    </FormControl>
 
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-							<FormField
-								control={form.control}
-								name="dueDate"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Due Date</FormLabel>
+              <FormField
+                control={form.control}
+                name="dueDate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Due Date</FormLabel>
 
-										<FormControl>
-											<DatePicker {...field} />
-										</FormControl>
+                    <FormControl>
+                      <DatePicker {...field} />
+                    </FormControl>
 
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-							<FormField
-								control={form.control}
-								name="sprint"
-								render={({ field }) => (
-								<FormItem>
-									<FormLabel>Sprint</FormLabel>
-									<FormControl>
-									<Input {...field} placeholder="Enter sprint details" />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-								)}
-							/>
-				
-							<FormField
-								control={form.control}
-								name="storyPoints"
-								render={({ field }) => (
-								<FormItem>
-									<FormLabel>Story Points</FormLabel>
-									<Select
-									onValueChange={field.onChange}
-									defaultValue={field.value}
-									>
-									<FormControl>
-										<SelectTrigger>
-										<SelectValue placeholder="Select story points" />
-										</SelectTrigger>
-									</FormControl>
-									<FormMessage />
-									<SelectContent>
-										{Object.entries(FibonacciSeries).map(([key, value]) => (
-										<SelectItem
-											key={value}
-											value={value.toString().toUpperCase()}
-										>
-											{key
-											.replace("_", " ")
-											.toLowerCase()
-											.replace(/\b\w/g, (char) => char.toUpperCase())}
-										</SelectItem>
-										))}
-										<FormMessage />
-									</SelectContent>
-									</Select>
-								</FormItem>
-								)}
-							/>
-				
-							<FormField
-								control={form.control}
-								name="priority"
-								render={({ field }) => (
-								<FormItem>
-									<FormLabel>Priority</FormLabel>
-									<Select
-									onValueChange={field.onChange}
-									defaultValue={field.value}
-									>
-									<FormControl>
-										<SelectTrigger>
-										<SelectValue placeholder="Select priority" />
-										</SelectTrigger>
-									</FormControl>
-									<FormMessage />
-									<SelectContent>
-										{Object.entries(Priority).map(([key, value]) => (
-										<SelectItem key={value} value={value.toUpperCase()}>
-											{key
-											.replace("_", " ")
-											.toLowerCase()
-											.replace(/\b\w/g, (char) => char.toUpperCase())}
-										</SelectItem>
-										))}
-										<FormMessage />
-									</SelectContent>
-									</Select>
-								</FormItem>
-								)}
-							/>				
+              <FormField
+                control={form.control}
+                name="sprint"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Sprint</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter sprint details" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-							<FormField
-								control={form.control}
-								name="assigneeId"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Assignee</FormLabel>
-										<Select
-											onValueChange={field.onChange}
-											defaultValue={field.value}
-										>
-											<FormControl>
-												<SelectTrigger>
-													<SelectValue placeholder="Select an assignee" />
-												</SelectTrigger>
-											</FormControl>
+              <FormField
+                control={form.control}
+                name="storyPoints"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Story Points</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select story points" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage />
+                      <SelectContent>
+                        {Object.entries(FibonacciSeries).map(([key, value]) => (
+                          <SelectItem
+                            key={value}
+                            value={value.toString().toUpperCase()}
+                          >
+                            {key
+                              .replace("_", " ")
+                              .toLowerCase()
+                              .replace(/\b\w/g, (char) => char.toUpperCase())}
+                          </SelectItem>
+                        ))}
+                        <FormMessage />
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
 
-											<FormMessage />
+              <FormField
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Priority</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select priority" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage />
+                      <SelectContent>
+                        {Object.entries(Priority).map(([key, value]) => (
+                          <SelectItem key={value} value={value.toUpperCase()}>
+                            {key
+                              .replace("_", " ")
+                              .toLowerCase()
+                              .replace(/\b\w/g, (char) => char.toUpperCase())}
+                          </SelectItem>
+                        ))}
+                        <FormMessage />
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
 
-											<SelectContent>
-												{memberOptions.map((member) => (
-													<SelectItem key={member.id} value={member.id}>
-														<div className="flex items-center gap-x-2">
-															<MemberAvatar
-																className="size-6"
-																name={member.name}
-															/>
-															{member.name}
-														</div>
-													</SelectItem>
-												))}
-											</SelectContent>
-										</Select>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="status"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Status</FormLabel>
+              <FormField
+                control={form.control}
+                name="assigneeId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Assignee</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select an assignee" />
+                        </SelectTrigger>
+                      </FormControl>
 
-										<Select
-											onValueChange={field.onChange}
-											defaultValue={field.value}
-										>
-											<FormControl>
-												<SelectTrigger>
-													<SelectValue placeholder="Select status" />
-												</SelectTrigger>
-											</FormControl>
-											
-											<FormMessage />
+                      <FormMessage />
 
-											<SelectContent>
-												{Object.entries(TaskStatus).map(([key, value]) => (
-													<SelectItem key={value} value={value.toUpperCase()}>
-														{key
-															.replace("_", " ")
-															.toLowerCase()
-															.replace(/\b\w/g, (char) => char.toUpperCase())}
-													</SelectItem>
-												))}
-											</SelectContent>
-										</Select>
+                      <SelectContent>
+                        {memberOptions.map((member) => (
+                          <SelectItem key={member.id} value={member.id}>
+                            <div className="flex items-center gap-x-2">
+                              <MemberAvatar
+                                className="size-6"
+                                name={member.name}
+                              />
+                              {member.name}
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Status</FormLabel>
 
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="projectId"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Project</FormLabel>
-										<Select
-											onValueChange={field.onChange}
-											defaultValue={field.value}
-										>
-											<FormControl>
-												<SelectTrigger>
-													<SelectValue placeholder="Select a project" />
-												</SelectTrigger>
-											</FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select status" />
+                        </SelectTrigger>
+                      </FormControl>
 
-											<FormMessage />
+                      <FormMessage />
 
-											<SelectContent>
-												{projectOptions.map((project) => (
-													<SelectItem key={project.id} value={project.id}>
-														<div className="flex items-center gap-x-2">
-															<ProjectAvatar
-																image={project.imageUrl}
-																className="size-6"
-																name={project.name}
-															/>
-															{project.name}
-														</div>
-													</SelectItem>
-												))}
-											</SelectContent>
-										</Select>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-						</div>
+                      <SelectContent>
+                        {Object.entries(TaskStatus).map(([key, value]) => (
+                          <SelectItem key={value} value={value.toUpperCase()}>
+                            {key
+                              .replace("_", " ")
+                              .toLowerCase()
+                              .replace(/\b\w/g, (char) => char.toUpperCase())}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
 
-						<DottedSeparator className="py-7" />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a project" />
+                        </SelectTrigger>
+                      </FormControl>
 
-						<div className="flex items-center justify-between">
-							<Button
-								type="button"
-								size="lg"
-								variant="secondary"
-								onClick={onCancel}
-								disabled={isPending}
-								className={cn(!onCancel && "invisible")}
-							>
-								Cancel
-							</Button>
+                      <FormMessage />
 
-							<Button 
-                            disabled={isPending} 
-                            type="submit" size="lg">
-								Save Changes
-							</Button>
-						</div>
-					</form>
-				</Form>
-			</CardContent>
-		</Card>
-	);
+                      <SelectContent>
+                        {projectOptions.map((project) => (
+                          <SelectItem key={project.id} value={project.id}>
+                            <div className="flex items-center gap-x-2">
+                              <ProjectAvatar
+                                image={project.imageUrl}
+                                className="size-6"
+                                name={project.name}
+                              />
+                              {project.name}
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <DottedSeparator className="py-7" />
+
+            <div className="flex items-center justify-between">
+              <Button
+                type="button"
+                size="lg"
+                variant="secondary"
+                onClick={onCancel}
+                disabled={isPending}
+                className={cn(!onCancel && "invisible")}
+              >
+                Cancel
+              </Button>
+
+              <Button disabled={isPending} type="submit" size="lg">
+                Save Changes
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
