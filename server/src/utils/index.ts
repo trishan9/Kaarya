5d8e21aa20a4b9d251eb5eba894317b04918b412
sushant696 <sushantprasaii7@gmail.com>
2025-01-21 20:13:43 +0545

    feat: added string utiltity functions

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -13,3 +13,13 @@ export const generateInviteCode = (length: number) => {
 };
 
 export const INVITECODE_LENGTH = 6;
+
+export const snakeCaseToTitleCase = (str: string) => {
+  return str
+    .toLowerCase()
+    .replace(/_/g, " ")
+    .replace(/\b\w/g, (letter) => letter.toUpperCase());
+};
+
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
