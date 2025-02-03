ab6a246107faa56e94015c7982a3f5e8c30f063d
Nischaymrzn <nischaymaharjann@gmail.com>
2025-02-03 20:53:37 +0545

    feat: Create Log Modal component

---
+++ client/src/pages/dashboard/projects/_components/CreateLogModal.tsx
@@ -0,0 +1,14 @@
+import { ResponsiveModal } from "@/components/ResponsiveModal"
+import { CreateLogFormWrapper } from "./CreateLogFormWrapper"
+import { useCreateLogModal } from "@/hooks/useCreateLogModal"
+
+export const CreateLogModal = () => {
+  const { isOpen, setIsOpen, close } = useCreateLogModal()
+
+  return (
+    <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
+      <CreateLogFormWrapper onCancel={close} />
+    </ResponsiveModal>
+  )
+}
+
